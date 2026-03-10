c50bf3ccc79a9f9ce7dbcf5681cb36252d0a31ec
EthanBrau <ethanbrau@gustavus.edu>
2026-03-09 19:06:20 -0500

    git not working this is what claude says

---
+++ src/CourseSatisfaction.java
@@ -0,0 +1,21 @@
+/**
+ * File: CourseSatisfaction.java
+ * Description: CourseSatisfaction enum for course specializations needed to be completed to graduate (Gustavus
+ *              Registration System)
+ *
+ * @author  Junior Opoku-Ansah (junioro@gustavus.edu)
+ * @version 1.0
+ * @since   2026-02-19
+ */
+
+
+public enum CourseSatisfaction {
+    QUANT,
+    USIDG,
+    GLAFC,
+    LANG101,
+    LANG102,
+    CHALS,
+    FTS,
+    SIGX,
+}
---
+++ src/LabTest.java
@@ -0,0 +1,71 @@
+/**
+ * File: LabTest.java
+ * Description: Course code for Lab subClass UML diagram for group midterm (Gustavus
+ *              Registration System)
+ *
+ * @author  Junior Opoku-Ansah (junioro@gustavus.edu)
+ * @version 1.0
+ * @since   2026-02-19
+ */
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LabTest {
+
+    private Lab lab;
+
+    @BeforeEach
+    void setUp() {
+        Professor prof = new Professor(12,"Jaden", "Lee");
+        Student taJames = new Student("James", "Jameson", 90);
+        Student taSam = new Student("Sam", "Samson", 43);
+        List<Student> tas = List.of(taJames, taSam);
+
+        lab = new Lab(
+                new ArrayList<Course>(),
+                "Chemistry Lab",
+                1400,
+                new ArrayList<Student>(),
+                "CHEM",
+                prof,
+                "Spring 2026",
+                20,
+                new ArrayList<Integer>(),
+                tas,
+                55501,
+                3,
+                CourseSatisfaction.GLAFC
+        );
+    }
+
+    @Test
+    void testHasLabIsAlwaysTrue() {
+        assertTrue(lab.hasLab());
+    }
+
+    @Test
+    void testGetLinkedClassID() {
+        assertEquals(55501, lab.getLinkedClassID());
+    }
+
+    @Test
+    void testSetLinkedClassID() {
+        lab.setLinkedClassID(99999);
+        assertEquals(99999, lab.getLinkedClassID());
+    }
+
+    @Test
+    void testLabIsInstanceOfCourse() {
+        assertInstanceOf(Course.class, lab);
+    }
+
+    @Test
+    void testCreditsInheritedCorrectly() {
+        assertEquals(3, lab.getCredits());
+    }
+}
